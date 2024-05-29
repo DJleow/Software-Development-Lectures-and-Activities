93a23df098c9200dfcc68038bc5a614545ddf141
DJleow <233527@students.aih.nsw.edu.au>
2024-05-29 21:02:31 +1000

    Create SESSION2.py
    import statistics stages

---
+++ SESSION2.py
@@ -0,0 +1,8 @@
+import statistics # import your modules
+# initializing list # this is a list li
+li = [1, 2, 3, 3, 2, 2, 1] # li is the variables in the list
+
+# using mean () to calculate average of list
+# elements
+print(statistics.mean(li)) # print the value of mean
+print(statistics.median(li))
