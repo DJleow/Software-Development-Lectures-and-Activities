ce0f801a7336c00a44fda994853cb22e1f33ee16
DJleow <233527@students.aih.nsw.edu.au>
2024-05-29 19:16:29 +1000

    Create Sales_Tax.py
    This is a normal sales tax for varieties of products.

---
+++ Sales_Tax.py
@@ -0,0 +1,15 @@
+phones = 1400 # 10, initializing first variable or product
+laptops = 2000 # 20, #variable
+watch = 1500 # 8, #variable
+#
+airpods = 200 #13, #variable
+tv = 6000 #8, #variable
+
+st_phones = phones*0.1
+st_laptops = laptops*0.2
+st_watch = watch*0.08
+st_airpods = airpods*0.13
+st_tv = tv*0.08
+
+print(st_phones, st_laptops, st_watch, st_airpods, st_tv)
+print(st_phones+phones, st_laptops+laptops, st_watch+watch, st_airpods+airpods, st_tv+tv)
